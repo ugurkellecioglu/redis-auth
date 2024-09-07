03ea128b3a2720da849a3728900a06b845ff85c3
Ugur <ugur.kellecioglu@outlook.com>
2024-09-07 10:39:39 +0300

    chore: Add signupUser and login functions to userService

---
+++ services/userService/index.ts
@@ -0,0 +1,42 @@
+"use server"
+import { signIn } from "@/lib/auth"
+import redis from "@/lib/redis"
+import bcryptjs from "bcryptjs"
+import { v4 as uuid } from "uuid"
+import { LoginInput } from "./types"
+export async function signupUser(registerInput: LoginInput) {
+  // check if user already exists
+  const userKey = `user:${registerInput.email}`
+  const userExists = await redis.exists(userKey)
+
+  if (userExists) {
+    throw new Error("User already exists")
+  }
+
+  // hash password using bcrypt
+  const hashedPassword = await bcryptjs.hash(registerInput.password, 10)
+  const id = uuid()
+
+  const user = {
+    id,
+    email: registerInput.email,
+  }
+
+  await redis.set(`user:${id}`, user)
+
+  // insert user into Redis
+  await redis.hmset(userKey, {
+    ...user,
+    password: hashedPassword,
+  })
+
+  return user
+}
+
+export function login({ email, password }: LoginInput) {
+  return signIn("credentials", {
+    redirect: false,
+    email,
+    password,
+  })
+}
---
+++ services/userService/types.ts
@@ -0,0 +1,7 @@
+// i'll use this type in the register as well
+// ideally we should have a confirm password field in the register form
+// but in this demo we'll keep it simple
+export interface LoginInput {
+  email: string
+  password: string
+}
