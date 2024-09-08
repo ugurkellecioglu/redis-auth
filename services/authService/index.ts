904834dc27424c9c1508b31535a085239af2b4d8
Ugur <ugur.kellecioglu@outlook.com>
2024-09-08 11:41:50 +0300

    refactor: Move user creation logic to data layer

--- services/authService/index.ts
+++ services/authService/index.ts
@@ -1,41 +1,22 @@
-"use server"
+import { createUser, findUserByEmail } from "@/data/userRepository"
 import { signIn } from "@/lib/auth"
-import { hashPassword } from "@/lib/hash"
-import redis from "@/lib/redis"
-import { v4 as uuid } from "uuid"
+import { hashPassword } from "@/utils/hash"
 import { LoginInput } from "./types"
 
 export async function signupUser(registerInput: LoginInput) {
-  // check if user already exists
-  const userKey = `user:${registerInput.email}`
-  const userExists = await redis.exists(userKey)
+  const existingUser = await findUserByEmail(registerInput.email)
 
-  if (userExists) {
+  if (existingUser) {
     throw new Error("User already exists")
   }
 
-  // hash password using bcrypt
   const hashedPassword = await hashPassword(registerInput.password)
-  const id = uuid()
-
-  const user = {
-    id,
-    email: registerInput.email,
-  }
-
-  await redis.set(`user:${id}`, user)
-
-  // insert user into Redis
-  await redis.hmset(userKey, {
-    ...user,
-    password: hashedPassword,
-  })
-
-  return user
+  return createUser(registerInput.email, hashedPassword)
 }
+
 export async function login({ email, password }: LoginInput) {
   return signIn("credentials", {
-    redirect: false,
+    redirect: false, // this is important because next 14 throws an error if we try to make a redirect in the server
     email,
     password,
   })
